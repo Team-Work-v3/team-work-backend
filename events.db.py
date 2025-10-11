44d557422413201d71b4f46b24819e55c02ed5fd
alesya <alesyamaksimuks@gmail.com>
2025-10-11 20:24:33 +0300

    add my brains

--- events.db.py
+++ events.db.py
@@ -1,62 +1,75 @@
 import sqlite3
 
-conn = sqlite3.connect('library_site.db')
-cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        login TEXT NOT NULL,
-                        password TEXT NOT NULL,
-                        role TEXT NOT NULL)''')
+class LibraryDBCreator:
+    def __init__(self, db_name='library_site.db'):
+        self.db_name = db_name
+        self.connector = sqlite3.connect(self.db_name)
+        self.cursor = self.connector.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS events (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        name TEXT NOT NULL,
-                        description TEXT,
-                        date TEXT NOT NULL,
-                        time TEXT NOT NULL,
-                        location TEXT NOT NULL,
-                        created_by INTEGER,
-                        FOREIGN KEY (created_by) REFERENCES users(id))''')
+    def createUsersTable(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                login TEXT NOT NULL,
+                                password TEXT NOT NULL,
+                                role TEXT NOT NULL)''')
+        self.connector.commit()
 
-conn.commit()
-conn.close()
+    def createEventsTable(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS events (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                description TEXT,
+                                date TEXT NOT NULL,
+                                time TEXT NOT NULL,
+                                location TEXT NOT NULL,
+                                created_by INTEGER,
+                                FOREIGN KEY (created_by) REFERENCES users(user_id))''')
+        self.connector.commit()
 
-def getUserID(user_id):
-    conn = sqlite3.connect('library_site.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT user_id FROM users WHERE user_id = ?', (user_id,))
-    result = cursor.fetchone()
-    conn.close()
-    if result:
-        return result[0]
-    return None
+    def __del__(self):
+        self.connector.close()
 
-def getUser(login):
-    conn = sqlite3.connect('library_site.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT login FROM users WHERE login = ?', (login,))
-    result = cursor.fetchone()
-    if result:
-        return result[0]
-    return None
+class LibraryDB:
+    def __init__(self, db_name='library_site.db'):
+        self.db_name = db_name
+        self.connector = sqlite3.connect(self.db_name)
+        self.cursor = self.connector.cursor()
 
-def addEvent(user_id, name, description, date, time, location):
+    def getUserID(self, user_id):
+        self.cursor.execute('SELECT user_id FROM users WHERE user_id = ?', (user_id,))
+        result = self.cursor.fetchone()
+        if result:
+            return result[0]
+        return None
 
-    conn = sqlite3.connect('library_site.db')
-    cursor = conn.cursor()
+    def getUser(self, login):
+        self.cursor.execute('SELECT login FROM users WHERE login = ?', (login,))
+        result = self.cursor.fetchone()
+        if result:
+            return result[0]
+        return None
 
-    cursor.execute('SELECT role FROM users WHERE user_id = ?',(user_id,))
-    result = cursor.fetchone()
+    def addEvent(self, user_id, name, description, date, time, location):
 
-    if not result or result[0] != 'admin':
-        conn.close()
-        return False
+        self.cursor.execute('SELECT role FROM users WHERE user_id = ?',(user_id,))
+        result = self.cursor.fetchone()
 
-    cursor.execute('''INSERT INTO events (name, description, date, time, location, created_by) 
-    VALUES (?, ?, ?, ?, ?, ?)''', (name, description, date, time, location, user_id))
+        if not result or result[0] != 'admin':
+            return False
 
-    conn.commit()
-    conn.close()
-    return True
+        self.cursor.execute('''INSERT INTO events (name, description, date, time, location, created_by) 
+        VALUES (?, ?, ?, ?, ?, ?)''', (name, description, date, time, location, user_id))
+
+        self.connector.commit()
+        return True
+
+    def __del__(self):
+        self.connector.close()
+
+
+if __name__ == "__main__":
+    LibraryDBCreator().createUsersTable()
+    LibraryDBCreator().createEventsTable()
+
 
