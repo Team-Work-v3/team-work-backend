2331ea9905d8b28fc1559f4d49e39bf178dde51d
VVK <vvkuprik@gmail.com>
2025-12-18 09:52:20 +0300

    fix mentors

--- temp_points.py
+++ temp_points.py
@@ -7,7 +7,7 @@ temp_points = Blueprint('temp_points', __name__, url_prefix='/temp')
 
 @temp_points.route("/getMentors", methods=["GET"])
 def get_Mentors():
-    path = "/home/images/mentors"
-    files = ["/temp/getMentor/" + f for f in os.listdir(path) if os.path.isfile(f)]
+    path = "/home/images/mentors/"
+    files = ["/temp/getMentor/" + f for f in os.listdir(path) if os.path.isfile(path + f)]
     return jsonify({"links_images": files})
 
