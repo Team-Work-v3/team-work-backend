b3f3668a9bdc23739a2d54e78042ed44806cd7e2
VVK <vvkuprik@gmail.com>
2025-12-18 09:48:55 +0300

    add mentors

--- temp_points.py
+++ temp_points.py
@@ -1,5 +1,13 @@
 from flask import Blueprint, jsonify
-
+import os
 
 
 temp_points = Blueprint('temp_points', __name__, url_prefix='/temp')
+
+
+@temp_points.route("/getMentors", methods=["GET"])
+def get_Mentors():
+    path = "/home/images/mentors"
+    files = ["/temp/getMentor/" + f for f in os.listdir(path) if os.path.isfile(f)]
+    jsonify({"links_images": files})
+
